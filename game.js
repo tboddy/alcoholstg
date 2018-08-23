f8c269570b0ae0c943f84708a77f6bcbdb5000d0
Trevor Boddy <bboddyy@gmail.com>
2018-08-23 19:27:02 -0400

    background overlay

--- game.js
+++ game.js
@@ -179,7 +179,7 @@ mapControls = () => {
 	document.addEventListener('keyup', keysUp);
 };
 const background = {
-	clock: 0,
+
 	bgTile: false,
 
 	draw(){
@@ -195,13 +195,26 @@ const background = {
 		background.bgTile.x = 0;
 		background.bgTile.y = 0;
 
+		const overlay = PIXI.Sprite.fromImage('img/overlay.png')
+		overlay.x = gameX;
+		overlay.y = gameY;
+		overlay.zIndex = 2;
+
 		game.stage.addChild(container);
 		container.addChild(background.bgTile);
+		game.stage.addChild(overlay);
 
 		const pos = container.toLocal({x: gameX + gameWidth / 2, y: -grid * 10}, undefined, undefined, undefined, PIXI.projection.TRANSFORM_STEP.BEFORE_PROJ);
 		pos.y = -pos.y;
 		pos.x = -pos.x;
 		container.proj.setAxisY(pos, -1);
+
+		// background.bgTile = new PIXI.extras.TilingSprite(new PIXI.Texture.fromImage('img/bg.png'));
+		// background.bgTile.width = gameWidth;
+		// background.bgTile.height = gameHeight;
+		// background.bgTile.x = gameX;
+		// background.bgTile.y = gameY;
+		// game.stage.addChild(background.bgTile)
 
 	},
 
